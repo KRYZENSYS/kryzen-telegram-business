f82b0c1ce2084efb605d85eba971e69485a2d7e3
KRYZENSYS <f91186645@gmail.com>
2026-07-06 14:45:28 +0500

    fix: add exceptions to utils init

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,4 @@
-"""Utility helpers."""
+"""Utility helpers (re-exported for convenience)."""
 from app.utils.helpers import (
     generate_id,
     mask_string,
@@ -17,10 +17,18 @@ from app.utils.validators import (
     validate_promo_code,
 )
 from app.utils.time import utcnow, format_dt, parse_dt
+from app.utils.exceptions import (
+    KryzenError, NotFoundError, AlreadyExistsError, ValidationError,
+    PermissionDeniedError, AuthenticationError, RateLimitError,
+    BusinessAPIError, AIProviderError, PremiumRequiredError,
+)
 
 __all__ = [
     "generate_id", "mask_string", "truncate", "format_bytes", "format_duration",
     "escape_html", "safe_int", "safe_float", "chunked",
     "is_valid_regex", "validate_telegram_id", "validate_username", "validate_promo_code",
     "utcnow", "format_dt", "parse_dt",
+    "KryzenError", "NotFoundError", "AlreadyExistsError", "ValidationError",
+    "PermissionDeniedError", "AuthenticationError", "RateLimitError",
+    "BusinessAPIError", "AIProviderError", "PremiumRequiredError",
 ]
