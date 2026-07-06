ff2c4fe7563d509445d0cc00b9a3ba50aee96ff8
KRYZENSYS <f91186645@gmail.com>
2026-07-06 15:55:37 +0500

    add services/__init__

---
+++ app/services/__init__.py
@@ -0,0 +1,10 @@
+from app.services.user import UserService
+from app.services.business import BusinessService
+from app.services.rule import RuleService
+from app.services.message import MessageService
+from app.services.promocode import PromoCodeService
+from app.services.ai import AIService
+from app.services.notification import NotificationService
+from app.services.subscription import SubscriptionService
+__all__ = ["UserService","BusinessService","RuleService","MessageService",
+           "PromoCodeService","AIService","NotificationService","SubscriptionService"]
